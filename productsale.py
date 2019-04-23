30cdef826f3e2401f214550f950cd23bf290853b
ynorouzz <ynr.info@gmail.com>
2019-04-23 15:33:55 +0200

    Load data file
    Load json data to dataframe

--- productsale.py
+++ productsale.py
@@ -6,8 +6,10 @@ import pandas as pd
 # import json
 # import numpy as np
 
+dataPath = './Data/'
 
-
+# Load json data to dataframe
+data = pd.read_json(dataPath+'20190207_transactions.json', orient='columns', lines=True)
 
 
 
