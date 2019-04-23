e6a48d6069578728a2eb290038d02ec0e40ca8ef
ynorouzz <ynr.info@gmail.com>
2019-04-23 17:01:16 +0200

    Move preprocessing function to new package

---
+++ data_preprocessing.py
@@ -0,0 +1,30 @@
+import glob
+import pandas as pd
+
+
+def integrate_data(dataPath):
+    """
+    Read json files from the directory and return data as dataframe
+
+    :param dataPath: the relative path of json data files
+    :return data_sef_df: a dataframe of json files with two columns (customer_id, list of product_ids)
+            products: a dataframe that is extracted all product_ids from data_sef_df
+    """
+    data_set = []
+
+    # List all json files
+    data_files = glob.glob(dataPath + "*.json")
+
+    # Concat json files into a list of dataframes
+    for json_file in data_files:
+        data = pd.read_json(json_file, orient='columns', lines=True)
+        data_set.append(data)
+
+    # Integrate dataframes into one dataframe
+    data_sef_df = pd.concat(data_set)
+
+    # Select product ids
+    products = pd.DataFrame(data_sef_df["products"].tolist())
+
+    return data_sef_df, products
+
