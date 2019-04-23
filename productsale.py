d0eac61f753d4461b6505f98b15d7db54d76ac77
ynorouzz <ynr.info@gmail.com>
2019-04-23 16:48:46 +0200

    Extend scripts to manage multiple json files

--- productsale.py
+++ productsale.py
@@ -17,3 +17,26 @@ products = pd.DataFrame(data["products"].tolist())
 # Print frequency of products
 print(' How many times a product  \n', products.count())
 
+################################ 
+# Extend to multiple json files
+################################
+
+data_set = []
+
+# List all json files
+data_files = glob.glob(dataPath + "*.json")
+
+# Concat json files into a list of dataframes
+for file in data_files:
+
+    data = pd.read_json(dataPath + '20190207_transactions.json', orient='columns', lines=True)
+    data_set.append(data)
+
+# Integrate dataframes into one dataframe    
+data_sef_df = pd.concat(data_set)
+
+# Select product ids
+products = pd.DataFrame(data_sef_df["products"].tolist())
+
+# Print frequency of products
+print(' How many times a product  \n', products.count())
