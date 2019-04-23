4faf40bd8c713b3588d9e9fcfa8a5a819f97a862
ynorouzz <ynr.info@gmail.com>
2019-04-23 15:35:11 +0200

    Select product ids and count them
    Put product ids in a new dataframe and count them

--- productsale.py
+++ productsale.py
@@ -11,5 +11,9 @@ dataPath = './Data/'
 # Load json data to dataframe
 data = pd.read_json(dataPath+'20190207_transactions.json', orient='columns', lines=True)
 
+# Select product ids
+products = pd.DataFrame(data["products"].tolist())
 
+# Print frequency of products
+print(' How many times a product  \n', products.count())
 
