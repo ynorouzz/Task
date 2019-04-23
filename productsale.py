ba66599397607f63ff1a07468aa60dc2c9d833e1
ynorouzz <ynr.info@gmail.com>
2019-04-23 16:57:22 +0200

    Put supporting multiple files into a function

--- productsale.py
+++ productsale.py
@@ -21,22 +21,33 @@ print(' How many times a product  \n', products.count())
 # Extend to multiple json files
 ################################
 
-data_set = []
+def integrate_data(dataPath):
+    """
+    Read json files from the directory and return data as dataframe
+     
+    :param dataPath: the relative path of json data files
+    :return data_sef_df: a dataframe of json files with two columns (customer_id, list of product_ids)
+            products: a dataframe that is extracted all product_ids from data_sef_df
+    """
+    data_set = []
 
-# List all json files
-data_files = glob.glob(dataPath + "*.json")
+    # List all json files
+    data_files = glob.glob(dataPath + "*.json")
 
-# Concat json files into a list of dataframes
-for json_file in data_files:
+    # Concat json files into a list of dataframes
+    for json_file in data_files:
+        data = pd.read_json(json_file, orient='columns', lines=True)
+        data_set.append(data)
 
-    data = pd.read_json(json_file, orient='columns', lines=True)
-    data_set.append(data)
+    # Integrate dataframes into one dataframe    
+    data_sef_df = pd.concat(data_set)
 
-# Integrate dataframes into one dataframe    
-data_sef_df = pd.concat(data_set)
+    # Select product ids
+    products = pd.DataFrame(data_sef_df["products"].tolist())
 
-# Select product ids
-products = pd.DataFrame(data_sef_df["products"].tolist())
+    return data_sef_df, products
 
-# Print frequency of products
+
+data_sef_df, products = integrate_data(dataPath)
+#  Print frequency of products
 print(' How many times a product  \n', products.count())
